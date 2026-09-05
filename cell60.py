593d7455165189a2799317076224c12b48fa76cf
nrensen <nathanael.git@polymorpheus.com>
2026-09-05 18:02:49 +0800

    cell60 out

--- cell60.py
+++ cell60.py
@@ -54,7 +54,7 @@ from cell import (
 # Configuration
 # -----------------------------------------------------------------------------
 
-mp.mp.dps = 60
+mp.mp.dps = 50
 
 C_PARAM = 13
 L_PARAM = mp.log(C_PARAM)
